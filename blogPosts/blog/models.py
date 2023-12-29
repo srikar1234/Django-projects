ea91601086d95eb6a080e04185c70fe440660504
Srikar Kale <srikar.kale01@gmail.com>
2023-12-29 15:54:48 +0530

    Added DB to Blog App

--- blogPosts/blog/models.py
+++ blogPosts/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+#users to post is a one to many relationship
+#Create post model
+class Post(models.Model):
+    #title field will be a char field with max length 100
+    title = models.CharField(max_length = 100)
+    #Textfield is same as charfield but with unrestricted amount of text
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    #Use foreign key for one to many post author relationship
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    #Magic methods or special methods 
+    def __str__(self):
+        return self.title
--- blogPosts/blog/views.py
+++ blogPosts/blog/views.py
@@ -1,37 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#current package importing
+from .models import Post
 # Create your views here.
 # Views always have to resppond as a HTTP Response or an exception
 #either return http response or else render (returns http response as default)
 
-#dummy data to view on the template.html file
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blogpost',
-        'content': 'Firstpost',
-        'date_posted':'abce, 2023'
-    } ,
-    {
-        'author': 'ABCDMS',
-        'title': 'Blogpost2',
-        'content': 'Secpost',
-        'date_posted':'abce, 2021'
-    },
+# dummy data to view on the template.html file
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blogpost',
+#         'content': 'Firstpost',
+#         'date_posted':'abce, 2023'
+#     } ,
+#     {
+#         'author': 'ABCDMS',
+#         'title': 'Blogpost2',
+#         'content': 'Secpost',
+#         'date_posted':'abce, 2021'
+#     },
 
-    {
-        'author': 'Mochicat',
-        'title': 'Blogpost3',
-        'content': 'Thirdpost',
-        'date_posted':'abce, 2024'
-    } 
-]
+#     {
+#         'author': 'Mochicat',
+#         'title': 'Blogpost3',
+#         'content': 'Thirdpost',
+#         'date_posted':'abce, 2024'
+#     } 
+# ]
 
 def home(request):
     #passing the dummy data using the context variable.
     # The HTML file accesses the context object contents using 'posts' key 
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     #Add context to function as an argument
     return render(request, 'blog/home.html', context)
